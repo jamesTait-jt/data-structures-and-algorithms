815fd5735233b7abe609b8554f8f7d23c830e25d
jmsweg <jt15435@my.bristol.ac.uk>
2017-06-21 09:57:48 +0000

    finished bubble sort

--- algorithms/sorting/bubble-sort/bubble-sort.py
+++ algorithms/sorting/bubble-sort/bubble-sort.py
@@ -1,25 +1,42 @@
-#TODO: reduce comparisons by forgetting already sorted elements.
-#TODO: Tidy up recursive.
-#TODO: Iterative.
-
-
-def AscBubbleSort(arr):
+def RECBubbleSort(arr, n):
     i = 0
-    j = 1
     swaps = 0
     comps = 0
     print(arr)
-    while j < len(arr):
-        if arr[i] > arr[j]:
-            arr[j], arr[i] = arr[i], arr[j]
+    while i + 1 < n:
+        if arr[i] > arr[i + 1]:
+            arr[i + 1], arr[i] = arr[i], arr[i + 1]
             swaps += 1
-            print(arr)
+        print(arr)
         comps += 1
         i += 1
-        j += 1
-    if swaps == 0 and comps == len(arr) - 1:
-        return arr
-    else:
-        AscBubbleSort(arr)
+    print(swaps, comps)
+    if not (swaps == 0 and comps == n - 1):
+        n -= 1
+        RECBubbleSort(arr, n)
+    return arr
 
-AscBubbleSort([4, 1, 2, 3, 10, 9, 4, 7, 6])
+srtd = RECBubbleSort([4, 2, 1, 3, 10, 9, 4, 7, 6], 9)
+print(srtd)
+
+def ITRBubbleSort(arr):
+    n = len(arr)
+    swaps = 0
+    comps = 0
+    while not (swaps == 0 and comps == n):
+        print(arr)
+        swaps = 0
+        comps = 0
+        for i in range(0, n - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swaps += 1
+            print(arr)
+            comps += 1
+        print(swaps, comps)
+        #if n > 5:
+        n -= 1
+    return arr
+
+srtd = ITRBubbleSort([4, 2, 1, 3, 10, 9, 4, 7, 6])
+print (srtd)
