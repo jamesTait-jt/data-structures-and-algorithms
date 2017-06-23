47417eaf82556c70951d3ccfce7847758d0170aa
jmsweg <jt15435@my.bristol.ac.uk>
2017-06-23 18:43:21 +0000

    bogosort in python

---
+++ algorithms/sorting/bogo-sort/BogoSort.py
@@ -0,0 +1,32 @@
+import random
+
+def checkSorted(arr):
+    sorted = True
+    for i in range(len(arr) - 1):
+        if arr[i+1] < arr[i]:
+            sorted = False
+            break
+    return sorted
+
+def generatePerms(arr):
+
+    if len(arr) <= 1:
+        yield arr
+
+    else:
+        for perm in generatePerms(arr[1:]):
+            for i in range(len(arr)):
+                yield perm[:i] + arr[0:1] + perm[i:]
+
+
+def bogoSort(arr):
+    perms = generatePerms(arr)
+    for perm in perms:
+        if checkSorted(perm):
+            return perm
+
+arr = []
+for i in range(10):
+    arr.append(random.randint(0, 1000))
+
+print(bogoSort(arr))
