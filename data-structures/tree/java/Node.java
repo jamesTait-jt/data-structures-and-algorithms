30891435a0080c3956282131f4a48d2391de313f
jmsweg <jt15435@my.bristol.ac.uk>
2017-06-26 18:40:15 +0000

    started tree in java

---
+++ data-structures/tree/java/Node.java
@@ -0,0 +1,12 @@
+public class Node<T> {
+
+    private T data;
+    private Node<T> parent;
+    private Node<T> left;
+    private Node<T> right;
+
+    public Node() {
+        
+    }
+
+}
---
+++ data-structures/tree/java/Tree.java
@@ -0,0 +1,9 @@
+public class Tree<T> {
+
+
+
+    public Tree() {
+        
+    }
+
+}
