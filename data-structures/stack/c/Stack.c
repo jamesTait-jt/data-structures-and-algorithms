ef9e9efb5e918b8784658eefb73767e3f752b7cc
jmsweg <jt15435@my.bristol.ac.uk>
2017-06-28 12:31:41 +0000

    more stack in c

--- data-structures/stack/c/Stack.c
+++ data-structures/stack/c/Stack.c
@@ -11,33 +11,50 @@ int tail = -1;
 void push(int n) {
     if (tail == -1) {
         head = 0;
-    }
-    else {
-        printf("%d  |  %d\n", head, tail);
+    } else if (tail == MAX) {
+        printf("Stack overflow\n");
+    } else {
         for (int i = tail + 1 ; i > head ; i--) {
             stack[i] = stack[i-1];
         }
     }
     stack[head] = n;
     tail++;
-
-     for (int i = head ; i <= tail ; i++) {
-        printf("%d", stack[i]);
-    }
-    printf("\n");
 }
 
 void pop() {
-    
+    if (tail == -1) {
+        printf("Stack underflow\n");
+    } else {
+        for(int i = head ; i < tail ; i++) {
+            stack[i] = stack[i+1];
+        }
+        tail--;
+    }
+}
+
+void printStack() {
+   for (int i = head ; i <= tail ; i++) {
+        printf("%d", stack[i]);
+    }
+    printf("\n");
 }
 
 int main(void) {
     push(1);
     push(2);
     push(3);
-    //for (int i = head ; i <= tail ; i++) {
-    //    printf("%d", stack[i]);
-    //}
+    
+    printStack();
+
+    pop();
+
+    printStack();
+
+    pop();
+    push(3);
+
+    printStack();
 
     return 0;
 }
